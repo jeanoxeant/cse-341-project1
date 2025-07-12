f337ac4522f3f500921efd4d357cda1ae4a565ca
jeanoxeant <oxe24001@byui.edu>
2025-07-12 01:43:11 -0400

    fixing error (connection to Mongodb.

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,9 +1,8 @@
-const ObjectId = require('mongodb');
 const mongodb = require('../data/database');
-//const ObjectId = require('mongodb').ObjectId;
+const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
-    const result = await mongodb.getDatabase('contacts').collection('contacts').find();
+    const result = await mongodb.getDatabase().db().collection('contacts').find();
     result.toArray().then((contacts) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(contacts);
@@ -13,7 +12,7 @@ const getAll = async (req, res) => {
 
 const getSingle = async (req, res) => {
     const contactId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().collection('contacts').find({_id: contactId});
+    const result = await mongodb.getDatabase().db().collection('contacts').find({_id: contactId});
     result.toArray().then((contacts) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(contacts[0]);
