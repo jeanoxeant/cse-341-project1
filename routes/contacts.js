f91491f2b5ef553d9173d331863177f5a16124cc
jeanoxeant <oxe24001@byui.edu>
2025-07-18 13:43:56 -0400

    Install swagger

--- routes/contacts.js
+++ routes/contacts.js
@@ -5,5 +5,9 @@ const contactsController = require('../controllers/contacts');
 
 router.get('/', contactsController.getAll);
 router.get('/:id', contactsController.getSingle);
+router.post('/:id', contactsController.createContact);
+router.put('/:id', contactsController.updateContact);
+router.delete('/:id', contactsController.deleteContact);
+
 
 module.exports = router;
